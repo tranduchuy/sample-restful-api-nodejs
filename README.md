128f9e433ea65e386fd6c528492960740d2282ec
tranduchuy <thewish.it@gmail.com>
2022-06-18 20:40:13 +0700

    feat: dotenv load env and readme

---
+++ README.md
@@ -0,0 +1,3 @@
+Create .env file with variables defined at .env.example. Then:
+1. `npm install`
+2. `npm start`
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const db = require('./db');
 const PORT = process.env.PORT || '3000';
